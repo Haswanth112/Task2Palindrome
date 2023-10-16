cb096da4b31a3621ad8bc01a6d90431ff0985751
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-10-16 15:57:17 +0530

    Add files via upload

---
+++ palindrome/Task2Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Palindrome{
+    public static void main(String args[])
+    {
+        System.out.print("Enter your string:");
+        Scanner sc = new Scanner(System.in);
+        String Reversestr ="";
+        String str;
+        str = sc.nextLine();
+        for(int i=0; i<str.length();i++){
+            Reversestr = str.charAt(i)+Reversestr;
+        }
+        System.out.println("Palindrome");
+        else
+        System.out.println("Not a palindrome");
+    }
+}
